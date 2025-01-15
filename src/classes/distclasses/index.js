794b9e2b95de0b8975f9d49ea36ef8961a32e9e3
PSkrabneu <skrebnev@gmail.com>
2025-01-15 16:33:19 +0100

    TypeScript class implemented

--- src/classes/distclasses/index.js
+++ src/classes/distclasses/index.js
@@ -10,26 +10,45 @@ var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
     return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
 };
-var _Player_score;
-class Player {
-    constructor() {
-        _Player_score.set(this, 0);
+var _PlayerTS_score, _PlayerTS_numLives;
+class PlayerTS {
+    constructor(first, last) {
+        _PlayerTS_score.set(this, 0);
+        _PlayerTS_numLives.set(this, 10);
+        this.first = first;
+        this.last = last;
     }
     taunt(phrase) {
         console.log(phrase);
     }
     get score() {
-        return __classPrivateFieldGet(this, _Player_score, "f");
+        return __classPrivateFieldGet(this, _PlayerTS_score, "f");
+    }
+    static randomPlayer() {
+        return new PlayerTS("", "");
     }
     set score(newScore) {
         if (newScore < 0) {
             throw new Error("Score cannot be negative");
         }
-        __classPrivateFieldSet(this, _Player_score, newScore, "f");
+        __classPrivateFieldSet(this, _PlayerTS_score, newScore, "f");
     }
 }
-_Player_score = new WeakMap();
-const player1 = new Player();
+_PlayerTS_score = new WeakMap(), _PlayerTS_numLives = new WeakMap();
+PlayerTS.description = "Playing in the game";
+class AdminPlayer extends PlayerTS {
+    constructor(first, last, powers) {
+        super(first, last);
+        this.isAdmin = true;
+        this.powers = powers;
+    }
+}
+const player1 = new PlayerTS("John", "Doe");
 console.log(player1.taunt("Hello! Player!!"));
+console.log(player1.first + " -> " + player1.last);
 player1.score = 78;
 console.log(player1.score);
+const adminPlayer1 = new AdminPlayer("AdminF", "AdminL", ["ban", "edit"]);
+console.log(adminPlayer1.taunt("Hello! Admin Player!!"));
+console.log(adminPlayer1.first + " -> " + adminPlayer1.last);
+console.log(typeof adminPlayer1);
--- src/classes/src/index.ts
+++ src/classes/src/index.ts
@@ -1,11 +1,24 @@
-class Player {
+class PlayerTS {
+  static description = "Playing in the game";
   #score = 0;
+  #numLives = 10;
+  first: string;
+  last: string;
+
+  constructor(first: string, last: string) {
+    this.first = first;
+    this.last = last;
+  }
   taunt(phrase: string) {
     console.log(phrase);
   }
 
   get score() {
     return this.#score;
+  }
+
+  static randomPlayer() {
+    return new PlayerTS("", "");
   }
 
   set score(newScore) {
@@ -16,7 +29,21 @@ class Player {
   }
 }
 
-const player1 = new Player();
+class AdminPlayer extends PlayerTS {
+  isAdmin = true;
+  powers: string[];
+  constructor(first: string, last: string, powers: string[]) {
+    super(first, last);
+    this.powers = powers;
+  }
+}
+
+const player1 = new PlayerTS("John", "Doe");
 console.log(player1.taunt("Hello! Player!!"));
+console.log(player1.first + " -> " + player1.last);
 player1.score = 78;
 console.log(player1.score);
+const adminPlayer1 = new AdminPlayer("AdminF", "AdminL", ["ban", "edit"]);
+console.log(adminPlayer1.taunt("Hello! Admin Player!!"));
+console.log(adminPlayer1.first + " -> " + adminPlayer1.last);
+console.log(typeof adminPlayer1);

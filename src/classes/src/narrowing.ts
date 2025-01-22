25d56860ee1199680ea0c4c1078c991d48d86b31
PSkrabneu <skrebnev@gmail.com>
2025-01-22 15:57:42 +0100

    Narrowing types

--- src/classes/src/narrowing.ts
+++ src/classes/src/narrowing.ts
@@ -90,3 +90,62 @@ function makeNoise(animal: Cat | Dog): string {
     return "Woof!";
   }
 }
+
+// Discriminated Unions
+interface Rooster {
+  name: string;
+  weight: number;
+  age: number;
+  kind: "rooster";
+}
+
+interface Cow {
+  name: string;
+  weight: number;
+  age: number;
+  kind: "cow";
+}
+
+interface Pig {
+  name: string;
+  weight: number;
+  age: number;
+  kind: "pig";
+}
+
+interface Sheep {
+  name: string;
+  weight: number;
+  age: number;
+  kind: "sheep";
+}
+
+type FarmAnimal = Pig | Rooster | Cow | Sheep;
+
+function getFarmAnimalSound(animal: FarmAnimal) {
+  switch (animal.kind) {
+    case "pig":
+      return "Oink!";
+    case "cow":
+      return "Moooo!";
+    case "rooster":
+      return "Cockadoodledoo!";
+    case "sheep":
+      return "Baaa!";
+    default:
+      // We should never make it here, if we handled all cases correctly
+      //   const shouldNeverGetHere: never = animal;
+      //   return shouldNeverGetHere
+      const _exhaustiveCheck: never = animal;
+      return _exhaustiveCheck;
+  }
+}
+
+const stevie: Rooster = {
+  name: "Stevie Chicks",
+  weight: 2,
+  age: 1.5,
+  kind: "rooster",
+};
+
+console.log(getFarmAnimalSound(stevie));
